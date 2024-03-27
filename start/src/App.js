9d604ded8f6c4bd8bdab3c218888af3d44784237
Annette Adhiambo <annetaliya@gmail.com>
2024-03-27 16:55:10 -0700

    qr-code generator

--- start/src/App.js
+++ start/src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import LoadMoreData from './components/Load-more-data';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import QRCodeGenerator from './components/qr-code-generator';
 import RandomColor from './components/randomColor';
 import StarRating from './components/star-rating';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data';
 
 function App() {
   return (
@@ -13,7 +16,9 @@ function App() {
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10}/> */}
       {/* <ImageSlider limit={'5'}/> */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+      {/* <TreeView  menus={menus}/> */}
+      <QRCodeGenerator />
     </div>
   );
 }
---
+++ start/src/components/qr-code-generator/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
+
+const QRCodeGenerator = () => {
+    const [qrCode, setQrCode] = useState('');
+    const [input, setInput] = useState('')
+
+    const handleGenerateQrCode = () => {
+        setQrCode(input)
+        setInput('')
+
+    }
+  return (
+    <div>
+        <h1>
+            QRCodeGenerator
+        </h1>
+        <div>
+            <input
+                type='text'
+                name='qr-code'
+                value='input'
+                placeholder='Enter your value here'
+                onChange={(e) => setInput(e.target.value)}
+             />
+            <button disable={input && input.trim() !== '' ? false : true} onClick={handleGenerateQrCode}>Generate</button>
+
+        </div>
+        <div>
+            <QRCode
+            id='qr-code-value'
+            value={qrCode}
+            size={400}
+            bgColor='#fff'
+
+             />
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default QRCodeGenerator
